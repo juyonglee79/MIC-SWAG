57c171b2a641a19041a6bc6ef6e0e23f28d4e125
juyonglee79 <37585586+juyonglee79@users.noreply.github.com>
2020-07-30 09:10:18 +0900

    [UPDATE] SignUpActivity ui

--- app/src/main/java/the/mic/swag/ui/activity/signup/SignUpActivity.java
+++ app/src/main/java/the/mic/swag/ui/activity/signup/SignUpActivity.java
@@ -1,4 +1,16 @@
 package the.mic.swag.ui.activity.signup;
 
-public class SignUpActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import the.mic.swag.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
 }
